370a6de12383b11f5e26cf80bd4bae7b92366f82
HanHeeGwon <gksgmlrnjs12@naver.com>
2023-09-06 19:35:59 +0900

    string

---
+++ 05_study/05_study.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    //string str1 = "zz";
+    //string str2 = "bbb";
+    //cout << (str1 > str2); //사전순 비교 후 길이비교
+
+    //cout << isalpha('a') << '\n'; //2를 반환한다.
+    //cout << isalpha('A') << '\n'; //1을 반환한다.
+    //cout << isalpha('@') << '\n'; //0을 반환한다.
+    
+    //string s = "Police say two people are two suspected of trying to create a shortcut two for their construction work.The two have been detained and the case is under further investigation.The 38-year-old man and 55-year-old woman were working near the affected area, the 32nd Great Wall.";
+    //cout << s.length() << '\n';
+    //cout << s.at(100) << '\n';
+    //int index=0;
+    //
+    //for (int i = 0; ; i++) {
+    //    index= s.find("two", index + i);
+    //    if (index == -1) break;
+    //    cout << i+1 << "번째 two 자리: " << index << "\n";
+    //}//모든 two 자리 찾아보기
+
+   /* string s1, s2;
+    string s;
+    stringstream stream;
+    bool flag;
+    while (true) {
+        flag = false;
+        cout << "두 문자열을 입력해줘\n";
+        cout << "s1:"; cin >> s1;
+        cout << "s2:"; cin >> s2;
+        
+     
+        
+        if (flag == true) continue;
+        cout << "전부 숫자야!\n";
+        break;
+    }
+
+    string s3;
+    int sum=0;
+    cout << "\n 두 숫자를 이어 붙여보자: ";
+    s3.append(s1); s3.append(s2);
+    cout << s3;
+    cout << "\n 두 숫자의 합을 출력해보자: ";
+    sum = stoi(s1) + stoi(s2);
+    cout << sum;*/
+    
+    /*string s = "Codingon";
+    s[0]=tolower(s[0]);
+    cout <<"소문자변경: " << s<<'\n';
+    string sub = s.substr(2, 4);
+    cout << "부분문자열 출력: " << sub << '\n';
+    for (int i = 0; i < sub.size(); i++) sub[i] = 'o';
+    s.replace(2, sub.size(), sub);
+    cout << "변경: " << s << '\n';
+    cout << "5->";
+    s.erase(2, 5);
+    cout << s;*/
+    
+    
+
+}
