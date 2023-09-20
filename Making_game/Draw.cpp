034b672c9cea9469bb14cd23856ae39fd0d4f964
hanheegwon <gksgmlrnjs12@naver.com>
2023-09-20 13:12:39 +0900

    Making_game

---
+++ Making_game/Draw.cpp
@@ -0,0 +1,17 @@
+#include "Draw.h"
+
+void Draw::DrawMainScreen() {
+	cout << "                                                 \n";
+	cout << "		#####    #####     #####                  \n";
+	cout << "		#    #   #    #   #                       \n";
+	cout << "		#####    #####    #   ###                 \n";
+	cout << "		#    #   #        #    #                  \n";
+	cout << "		#    #   #         #####                  \n";
+	cout << "			1: 게임 시작                          \n";
+	cout << "					                              \n";
+}
+
+
+void Draw::DrawMap() {
+		
+}
